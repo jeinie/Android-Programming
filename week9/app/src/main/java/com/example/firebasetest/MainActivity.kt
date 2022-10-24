c50a1f0277f8b49e6f3207d8a61c1ebf653659c3
장정윤 <jeinie9179@naver.com>
2022-10-24 20:33:12 +0900

    firebase-auth 실습

--- week9/app/src/main/java/com/example/firebasetest/MainActivity.kt
+++ week9/app/src/main/java/com/example/firebasetest/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.firebasetest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.firebasetest.databinding.ActivityMainBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if(Firebase.auth.currentUser == null) {
+            startActivity(
+                Intent(this, LoginActivity::class.java)
+            )
+            finish()
+        }
+        binding.textUID.text = Firebase.auth.currentUser?.uid ?: "No User"
+        binding.buttonSignout.setOnClickListener {
+            Firebase.auth.signOut()
+            finish()
+        }
     }
 }
